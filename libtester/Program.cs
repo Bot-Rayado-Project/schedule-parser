fdcdf3892840f455db70943a4e808c0b5d546698
3xiced <3xiced@gmail.com>
2022-07-16 18:48:06 +0600

    Initial commit before transfering to c#

---
+++ libtester/Program.cs
@@ -0,0 +1,5 @@
+﻿using static parser.TableDownloader;
+
+var bookLinks = GetBookLinks("https://mtuci.ru/time-table/");
+Console.WriteLine("Found {0} links", bookLinks.Count);
+bookLinks.ForEach(x => Console.WriteLine(x));
