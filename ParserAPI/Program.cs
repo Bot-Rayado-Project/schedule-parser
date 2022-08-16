c0db82ac6c64f17d1867d5113690ebcc4e281f0d
3xiced <3xiced@gmail.com>
2022-08-16 20:25:41 +0600

    Small changes

--- ParserAPI/Program.cs
+++ ParserAPI/Program.cs
@@ -93,27 +93,17 @@ parser.OnNewData += (object _, string[] data) =>
     }
 };
 
-app.MapGet("/api/v1/start", (bool runOnce) =>
-{
-    if (runOnce) return RunOnce();
-    else return RunForever();
-});
+app.MapGet("/api/v1/start", (bool runOnce) => runOnce ? RunOnce() : RunForever());
 
-app.MapGet("/api/v1/state", () =>
-{
-    return Results.Ok(new { State = state.ToString() });
-});
+app.MapGet("/api/v1/state", () => Results.Ok(new { State = state.ToString() }));
 
-app.MapGet("/api/v1/stop", () =>
-{
-    return Stop();
-});
+app.MapGet("/api/v1/stop", Stop);
 
 app.Run();
 
 IResult RunOnce()
 {
-    if (state == ParserStates.isStopped || state == ParserStates.isSuspended)
+    if (state is ParserStates.isStopped or ParserStates.isSuspended)
     {
         state = ParserStates.isRunningOnce;
         RunOnceAsync();
